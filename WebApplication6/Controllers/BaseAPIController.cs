9579646bdd956f6ed8d7fc7a78dad5bf0b46e3e6
jhon <464479460@qq.com>
2017-08-06 17:51:42 +0800

    添加项目文件。

---
+++ WebApplication6/Controllers/BaseAPIController.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using WebApplication6.DBContext;
+
+namespace WebApplication6.Controllers
+{
+    public class BaseAPIController : ApiController
+    {
+        protected readonly UserDBEntities UserDB = new UserDBEntities();
+        protected HttpResponseMessage ToJson(dynamic obj)
+        {
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
+            return response;
+        }
+    }
+}
---
+++ WebApplication6/app/app.module.ts
@@ -0,0 +1,14 @@
+﻿import { NgModule } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+@NgModule({
+    imports: [BrowserModule, ReactiveFormsModule, HttpModule],
+    declarations: [],
+    providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
+    bootstrap: []
+})
+
+export class AppModule { }
